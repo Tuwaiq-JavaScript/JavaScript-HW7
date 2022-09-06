00e50097a02b321f02f00896d62218e167c87b42
shahad <shahad_1hamad@hotmail.com>
2022-09-06 08:37:15 +0300

    shahad almania

---
+++ java/Contact.java
@@ -0,0 +1,13 @@
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.UUID;
+
+@AllArgsConstructor @Data
+public class Contact {
+    private UUID id;
+    private String name;
+    private String number;
+}
